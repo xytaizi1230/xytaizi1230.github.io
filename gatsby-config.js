d7b60646e495f5cd9eecf5a4b47f48ddee9db82c
常俊超 <changjunchao@bytedance.com>
2025-01-07 14:59:13 +0800

    feat: init

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `xytaizi1230`,
+    description: `欢迎光临`,
+    author: `@xytaizi1230`,
+    siteUrl: `https://xytaizi1230.github.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
